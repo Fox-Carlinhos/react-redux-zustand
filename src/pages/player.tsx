0c9402fa779078e3f4fabd952174ed2f7909fc9f
Fox-Carlinhos <carlosalberto.cfjr@gmail.com>
2023-06-25 13:05:03 -0300

    created style based on active lesson

--- src/pages/player.tsx
+++ src/pages/player.tsx
@@ -4,10 +4,17 @@ import { Header } from "../components/Header";
 import { Video } from "../components/Video";
 import { Module } from "../components/Module";
 import { useAppSelector } from "../store";
+import { useCurrentLesson } from "../store/player";
+import { useEffect } from "react";
 
 export function Player() {
   const modules = useAppSelector((state) => state.player.course.modules);
 
+  const { currentLesson } = useCurrentLesson();
+
+  useEffect(() => {
+    document.title = `Assistindo ${currentLesson.title}`;
+  }, [currentLesson]);
   return (
     <div className="h-screen bg-zinc-950 text-zinc-50 flex justify-center items-center">
       <div className="flex w-[1180px] flex-col gap-6">
