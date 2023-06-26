19cc309106e8a0e46a2692e0526fa300a422f006
Fox-Carlinhos <carlosalberto.cfjr@gmail.com>
2023-06-26 15:27:48 -0300

    migrate from redux to zustand n added unit tests

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,10 @@
-import { useAppSelector } from "../store";
-import { useCurrentLesson } from "../store/player";
+import { useCurrentLesson, useStore } from "../zustand-store";
 
 export function Header() {
+  const isLoading = useStore((store) => store.isLoading);
   const { currentModule, currentLesson } = useCurrentLesson();
-  const isCurseLoading = useAppSelector((state) => state.player.isLoading);
 
-  if (isCurseLoading) {
+  if (isLoading) {
     return <h1 className="text-2xl font-bold">Carregando...</h1>;
   }
 
--- src/pages/player.tsx
+++ src/pages/player.tsx
@@ -3,24 +3,27 @@ import { MessageCircle } from "lucide-react";
 import { Header } from "../components/Header";
 import { Video } from "../components/Video";
 import { Module } from "../components/Module";
-import { useAppDispatch, useAppSelector } from "../store";
-import { loadCourse, useCurrentLesson } from "../store/player";
 import { useEffect } from "react";
+import { useCurrentLesson, useStore } from "../zustand-store";
 
 export function Player() {
-  const dispatch = useAppDispatch();
-
-  const modules = useAppSelector((state) => state.player.course?.modules);
+  const { course, load } = useStore((store) => {
+    return {
+      course: store.course,
+      load: store.load,
+    };
+  });
 
   const { currentLesson } = useCurrentLesson();
 
   useEffect(() => {
-    dispatch(loadCourse());
+    load();
   }, []);
 
   useEffect(() => {
     document.title = `Assistindo ${currentLesson?.title}`;
   }, [currentLesson]);
+
   return (
     <div className="h-screen bg-zinc-950 text-zinc-50 flex justify-center items-center">
       <div className="flex w-[1180px] flex-col gap-6">
@@ -35,10 +38,11 @@ export function Player() {
 
         <main className="relative flex overflow-hidden rounded-lg border border-zinc-800 bg-zinc-900 shadow pr-80">
           <div className="flex-1">
+            {" "}
             <Video />
           </div>
           <aside className="w-80 absolute top-0 bottom-0 right-0 border-l divide-y-2 divide-zinc-900 border-zinc-800 bg-zinc-900 overflow-y-scroll scrollbar-thin scrollbar-track-zinc-950 scrollbar-thumb-zinc-800">
-            {modules && modules.map((module, index) => <Module key={module.id} moduleIndex={index} title={module.title} amountOfLesson={module.lessons.length} />)}
+            {course?.modules && course.modules.map((module, index) => <Module key={module.id} moduleIndex={index} title={module.title} amountOfLesson={module.lessons.length} />)}
           </aside>
         </main>
       </div>
